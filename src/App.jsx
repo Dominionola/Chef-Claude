99c79dc21d263cdf39fcff79c645ff4d8dbcc462
unknown <theoladejitoluwa@gmail.com>
2025-07-10 12:32:13 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import Header from "./Header";
+import Main1 from "./Main1";
+export default function App() {
+  return (
+    <>
+      <Header />
+      <Main1 />
+    </>
+  );
+}
---
+++ src/Main1.jsx
@@ -0,0 +1,14 @@
+export default function Main1() {
+  return (
+    <main>
+      <form className="add-ingredient-form">
+        <input
+          type="text"
+          placeholder="e.g. oregano"
+          aria-label="Add ingredient"
+        />
+        <button>Add Ingredient</button>
+      </form>
+    </main>
+  );
+}
